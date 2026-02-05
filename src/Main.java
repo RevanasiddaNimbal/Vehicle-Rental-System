ebf6d042b8fe077219c16ccef0f5dd679ade100b
Revanasidda Nimbal <revanasiddanimbal82@gmail.com>
2026-02-06 02:18:02 +0530

    added documentation part

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import Controller.showDocumentation;
+
+import java.util.Scanner;
+
 public class Main{
+
     public static void main(String[] args){
-        System.out.println("welcome to the vehicle rental system mini project:");
+        Scanner input = new Scanner(System.in);
+        showDocumentation documentation = new showDocumentation();
+          int Option;
+
+        System.out.println("\n========================================================================");
+        System.out.println("           WELCOME TO VEHICLE RENTAL MANAGEMENT SYSTEM                 ");
+        System.out.println("=========================================================================");
+
+        do {
+            // Displaying the options of the Rental system.
+            System.out.println("\n⭐ AVAILABLE OPTIONS ⭐");
+            System.out.println("-------------------------");
+
+            System.out.println("\nDOCUMENTATION");
+            System.out.println("  1. View Documentation");
+
+
+            System.out.print("\nChoose a command option: ");
+             Option = input.nextInt();
+             input.nextLine();
+         switch (Option){
+             case 1:  documentation.display();
+             break;
+             default : break;
+         }
+        }while(Option!=0);
     }
+
 }
